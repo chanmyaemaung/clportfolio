8bfd12bbc33b277e3420c2e744b2c6072380aca4
Chen Lay <maungmaungyoker.mmy@gmail.com>
2025-05-05 00:19:33 +0700

    feat: add admin user creation and logging in DatabaseSeeder
    - Created an admin user with specified credentials in DatabaseSeeder.
    - Added logging for admin user creation to track the process.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,14 +5,24 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        $adminUser = User::create([
+            'name' => 'Chan Lay',
+            'email' => env('FILAMENT_DASHBOARD_ADMIN_EMAIL'),
+            'password' => bcrypt(env('FILAMENT_DASHBOARD_ADMIN_PASSWORD')),
+        ]);
+
+        Log::info('Admin User Created: ', ['name' => $adminUser->name, 'email' => $adminUser->email]);
+
         $this->call([
             ProjectTechnologySeeder::class,
             ProjectSeeder::class,
